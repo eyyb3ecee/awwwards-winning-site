025ff64c15967002ba210e5ffc8e05b5ec473097
AVC <cresencioabcdef26@gmail.com>
2025-05-20 23:40:01 +0800

    Imported the GSAP and animated the clicking feature of the hero section per video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Button } from "./Button.jsx";
 import { TiLocation, TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -25,6 +26,32 @@ const Hero = () => {
 
     setCurrentIndex(upcomingVideoIndex);
   };
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
